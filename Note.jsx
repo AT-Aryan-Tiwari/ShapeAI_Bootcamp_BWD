96976d7235932d5363367126e42ccdf1371a5fa5
AT-Aryan-Tiwari <46364979+AT-Aryan-Tiwari@users.noreply.github.com>
2021-06-06 01:40:42 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>JS and Web Development</h1>
+      <p>I loved the workshop</p>
+    </div>
+  );
+}
+export default Note;
